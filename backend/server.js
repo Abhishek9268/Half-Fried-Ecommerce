0ac006b51d6c463d0022c6dfe797f40d7b29d71d
Abhishek <Abhishekchaurasia5201@gmail.com>
2026-05-27 12:28:58 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -26,4 +26,7 @@ app.use((err, req, res, next) => {
 });
 
 const PORT = process.env.PORT || 5000;
+app.get("/", (req, res) => {
+  res.send("Half Fried Ecommerce Backend is running");
+});
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
